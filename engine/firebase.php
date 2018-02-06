acf9849621b3f1e4a6a34439df72e2ac259374ce
Florian Leroux <leroux34u@iutsd-127-14.ad.univ-lorraine.fr>
2018-02-06 17:59:45 +0100

    Fix Facebook Auth

---
+++ engine/firebase.php
@@ -0,0 +1,8 @@
+<?php
+
+    const DEFAULT_URL = 'https://r8f6jdmzsvjy.firebaseio.com';
+    const DEFAULT_TOKEN = 'AIzaSyD0MmF-MK1JM6k3k2fcpHsbXPBtmz4Uc7U';
+    const DEFAULT_PATH = '/';
+
+    $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+?>
